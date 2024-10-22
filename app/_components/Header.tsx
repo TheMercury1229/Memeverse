6bb8abcb2794550c5dddc49621c7b1269a082e76
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-10-22 14:29:20 +0530

    finalilising  and ready in production

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -20,7 +20,7 @@ export async function Header() {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <FilePen className="h-6 w-6" />
@@ -51,7 +51,7 @@ export async function Header() {
         <SheetContent side="left">
           <nav className="grid gap-6 text-lg font-medium">
             <Link
-              href="#"
+              href="/"
               className="flex items-center gap-2 text-lg font-semibold"
             >
               <FilePen className="h-6 w-6" />
