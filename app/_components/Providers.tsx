2f9a63d6b60cd4b895c7dec16fc3e712e8e09f83
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-10-22 13:16:47 +0530

    fixed the error for upload

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -2,38 +2,44 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { ImageKitProvider } from "imagekitio-next";
 
-export const Providers = ({children}: {children: React.ReactNode}) => { 
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-const authenticator = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/api/auth");
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+  const authenticator = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXTAUTH_URL}/api/auth` ||
+          "http://localhost:3000/api/auth"
       );
-    }
 
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error: any) {
-    throw new Error(`Authentication request failed: ${error.message}`);
-  }
-};
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Request failed with status ${response.status}: ${errorText}`
+        );
+      }
+
+      const data = await response.json();
+      const { signature, expire, token } = data;
+      return { signature, expire, token };
+    } catch (error: any) {
+      throw new Error(`Authentication request failed: ${error.message}`);
+    }
+  };
 
   return (
     <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-
-    <ImageKitProvider urlEndpoint={process.env.NEXT_PUBLIC_URL_ENDPOINT!} publicKey={publicKey} authenticator={authenticator}>
-      {children}
-    </ImageKitProvider>
-          </ThemeProvider>
-  )
-}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ImageKitProvider
+        urlEndpoint={process.env.NEXT_PUBLIC_URL_ENDPOINT!}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        {children}
+      </ImageKitProvider>
+    </ThemeProvider>
+  );
+};
